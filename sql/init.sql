57b834578e1b8365fc2bb9a1aa45438104a3ee61
doriath17 <kep171024@gmail.com>
2026-02-16 06:22:09 +0100

    aggiunto dizionario delle associazioni

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,16 @@
 -- init.sql
+
+-- questo file è usato per inizializzare il database, creando tutte le tabelle e i vincoli necessari
+-- utilizza i comandi di psql per eseguire i file SQL che definiscono le tabelle e i vincoli
+-- per questo motivo non è portabile perché è pensato appositamente per inizializzare un database PostgreSQL
+-- usando psql. Un alternativa sarebbe quella di creare un unico script SQL. 
+
+-- si può usare il comando \i init.sql dalla shell interattiva di psql, lanciandolo dalla cartella sql. 
+
+-- fa fermare l'esecuzione in caso di errori al primo errore
 \set ON_ERROR_STOP on
 
+-- inizializza le tabelle
 \i ./tables/table_utente.sql
 \i ./tables/table_orto.sql
 \i ./tables/table_lotto.sql
@@ -10,6 +20,7 @@
 \i ./tables/table_attivita.sql
 \i ./tables/table_notifica.sql
 
+-- inizializza i vincoli
 \i ./constraints/constraints_orto.sql
 \i ./constraints/constraints_lotto.sql
 \i ./constraints/constraints_attivita.sql
